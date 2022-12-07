ddd6cedb909127dce9dc485421bd2677b90d20ca
RichZhou1999 <373072096@qq.com>
2022-12-06 22:40:55 -0800

    1

--- Mandelbrot.h
+++ Mandelbrot.h
@@ -13,11 +13,11 @@
 This function returns the number of iterations that cause the initial point to exceed the threshold.
 If the threshold is not exceeded after maxiters, the function returns maxiters.
 */
-u_int64_t MandelbrotIterations(u_int64_t maxiters, ComplexNumber * point, double threshold);
+u_int64_t MandelbrotIterations(long maxiters, ComplexNumber * point, double threshold);
 
 /*
 This function calculates the Mandelbrot plot and stores the result in output.
 The number of pixels in the image is resolution * 2 + 1 in one row/column. It's a square image.
 Scale is the the distance between center and the top pixel in one dimension.
 */
-void Mandelbrot(double threshold, u_int64_t max_iterations, ComplexNumber* center, double scale, u_int64_t resolution, u_int64_t * output);
+void Mandelbrot(double threshold, long max_iterations, ComplexNumber* center, double scale, long resolution, long * output);
